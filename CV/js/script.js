5b5f7bc95f321d261edae8f01e77193c437f4674
Isaac José Murillo Muñoz <103013971+Imurillo07@users.noreply.github.com>
2025-07-29 22:55:02 -0600

    version worked
    worked about two weeks on this version but still not final

---
+++ CV/js/script.js
@@ -0,0 +1,7 @@
+//typing animation
+var typed = new Typed(".typing", {
+    strings:["Frontend Developer", "Backend Developer", "Fullstack Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
